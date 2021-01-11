f17ed7cf6be57714ff5180ac2a58be4065d034c3
Christian <christian.gernsoe@gmail.com>
2021-01-11 15:52:05 +0100

    Added server/client files

---
+++ src/main/java/Team29/BubbleShooters/Server.java
@@ -0,0 +1,23 @@
+package Team29.BubbleShooters;
+
+/*
+ * Server checks that the name is valid (not empty, unique and not too long) 
+ * and creates a new room, if the roomID doesn't exist already (Lobby/butler method)
+ * 
+ * user.(name, roomID) -> server.credentials(name, roomID)
+ * server.("roomURI", name, roomID, roomURI) -> user.response("roomURI", name, roomID, roomURI)
+ *  
+ *  User puts movement/controls into repo. -> 
+ *  Server gets user controls and updated positions etc. ->
+ *  Server puts updated player/ball/arrow positions and collision info. -> 
+ *  User gets position and collision info.
+ *   
+ */ 
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        
+    }
+}
